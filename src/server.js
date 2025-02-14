79ea948696de03c0a398582c578c9d7b26ea32d7
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2025-02-14 11:13:24 +0200

    chore: add health and root endpoints

--- src/server.js
+++ src/server.js
@@ -15,7 +15,22 @@ app.use( cors() );
 app.use( express.json() );
 app.use( express.urlencoded( { extended: false } ) );
 
-
+app.get( '/', ( req, res ) => {
+    res.status( 200 ).json( {
+        message: 'Welcome to Story Time',
+        version: '1.0.0',
+        endpoints: {
+            auth: {
+                register: 'POST /api/auth/register',
+                login: 'POST /api/auth/login'
+            },
+            health: 'GET /health'
+        }
+    } );
+} );
+app.get( '/health', ( req, res ) => {
+    res.status( 200 ).json( { status: 'UP' } );
+} );
 app.use( '/api/auth', auth );
 
 app.listen( port, () => console.log( info( `Server is running on port ${ port }...` ) ) );
